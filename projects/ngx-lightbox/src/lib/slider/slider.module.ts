992ccc1ac15a06d2f6bcb389240c94120234068f
HuiiBuh <nhaderer1@gmail.com>
2020-08-26 17:41:55 +0200

    Some minor refactoring

--- projects/ngx-lightbox/src/lib/slider/slider.module.ts
+++ projects/ngx-lightbox/src/lib/slider/slider.module.ts
@@ -2,10 +2,11 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {SliderComponent} from './slider/slider.component';
 import { ControlsComponent } from './controls/controls.component';
+import { ShareComponent } from './share/share.component';
 
 
 @NgModule({
-  declarations: [SliderComponent, ControlsComponent],
+  declarations: [SliderComponent, ControlsComponent, ShareComponent],
   imports: [
     CommonModule
   ],
