39534bab6a77f70187c2738222c0d505ae3464ae
HuiiBuh <nhaderer1@gmail.com>
2020-08-29 15:19:51 +0200

    Added animation to share

--- projects/ngx-lightbox/src/lib/slider/share/share.component.ts
+++ projects/ngx-lightbox/src/lib/slider/share/share.component.ts
@@ -1,3 +1,4 @@
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import {Component, Input, OnDestroy, OnInit, Renderer2} from '@angular/core';
 import {TShareOptionList} from '../slider-interfaces';
 import {ShareService} from './share.service';
@@ -6,12 +7,28 @@ import {ShareService} from './share.service';
   selector: 'lib-share[shareOptionList]',
   templateUrl: './share.component.html',
   styleUrls: ['./share.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        opacity: 1,
+        transform: 'translateY(0)',
+      })),
+      state('closed', style({
+        opacity: 0,
+        transform: 'translateY(-10px)',
+      })),
+      transition('open <=> closed', [
+        animate('333ms cubic-bezier(0.4, 0, 0.22, 1)'),
+      ]),
+    ]),
+  ],
 })
 export class ShareComponent implements OnInit, OnDestroy {
 
 
   @Input() shareOptionList: TShareOptionList = [];
-
+  public display: 'block' | 'none' = 'none';
   private unsubscribable: () => void;
 
   constructor(private renderer2: Renderer2, public shareService: ShareService) {
