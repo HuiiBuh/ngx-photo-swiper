3d21e8de36b27fccae1a831cbbc792f5ff46f64c
HuiiBuh <nhaderer1@gmail.com>
2021-02-11 17:17:18 +0100

    Added the square gallery

--- projects/ngx-photo-swiper/src/lib/galleries/square-gallery/sqare-gallery/square-gallery.component.ts
+++ projects/ngx-photo-swiper/src/lib/galleries/square-gallery/sqare-gallery/square-gallery.component.ts
@@ -1,10 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IImage } from '../../../models/gallery';
+import { NgxLightboxService } from '../../../ngx-lightbox.service';
+import { LightboxStore } from '../../../store/lightbox.store';
 
 @Component({
-  selector: 'photo-square-gallery',
+  selector: 'photo-square-gallery[lightboxID]',
   templateUrl: './square-gallery.component.html',
-  styleUrls: ['./square-gallery.component.scss']
+  styleUrls: ['./square-gallery.component.scss'],
 })
-export class SquareGalleryComponent  {
+export class SquareGalleryComponent implements OnInit {
 
+  @Input() public lightboxID!: string;
+  public imageList!: Observable<IImage[]>;
+  public sliderActive!: Observable<boolean>;
+
+  constructor(private ngxLightboxService: NgxLightboxService, private store: LightboxStore) {
+  }
+
+  public ngOnInit(): void {
+    this.imageList = this.store.onChanges<IImage[]>('gallery', this.lightboxID);
+    this.sliderActive = this.store.onChanges<boolean>('slider', 'active');
+  }
+
+  public loadSlider(imageIndex: number): void {
+    this.ngxLightboxService.loadIndexInSlider(imageIndex, this.lightboxID);
+  }
 }
--- projects/ngx-photo-swiper/src/lib/galleries/square-gallery/square-gallery.module.ts
+++ projects/ngx-photo-swiper/src/lib/galleries/square-gallery/square-gallery.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SliderModule } from '../../slider/slider.module';
 import { SquareGalleryComponent } from './sqare-gallery/square-gallery.component';
 
 @NgModule({
   declarations: [SquareGalleryComponent],
-  imports: [
-    CommonModule,
-  ],
+    imports: [
+        CommonModule,
+        SliderModule,
+    ],
   exports: [SquareGalleryComponent],
 })
 export class SquareGalleryModule {
