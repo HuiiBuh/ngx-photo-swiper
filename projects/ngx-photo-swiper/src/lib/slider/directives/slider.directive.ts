0af705f6f135f0f0e02245f1f627eac7c9ae78f5
HuiiBuh <nhaderer1@gmail.com>
2021-03-30 17:57:56 +0200

    Removed logging statement

--- projects/ngx-photo-swiper/src/lib/slider/directives/slider.directive.ts
+++ projects/ngx-photo-swiper/src/lib/slider/directives/slider.directive.ts
@@ -45,7 +45,6 @@ export class SliderDirective implements OnInit, OnDestroy {
   }
 
   private showSlider(): void {
-    console.log(this.imageIndex);
     // tslint:disable-next-line:no-non-null-assertion
     this.store.updateSlider({imageIndex: this.imageIndex!, lightboxID: this.lightboxID!, active: true});
   }
